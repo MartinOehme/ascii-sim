bc0ca21aa32f42f254fd925756e2ca05308e4b8c
erikweissgerber <erikweissgerber@gmail.com>
2019-11-30 00:06:14 +0100

    added rudemmentary customer and customer orders

--- asciisim/sprites/sprite_enums.py
+++ asciisim/sprites/sprite_enums.py
@@ -6,8 +6,23 @@ class States(Enum):
     IN_USE = 1
     BROKEN = 2
 
+
 class CoffeeStates(Enum):
     CLEAR_COFFEE = 0
     CLEAR_WATER = 1
     REFILL_MILK = 2
     REFILL_WATER = 3
+
+
+class OrderWalkers(Enum):
+    HOT_DRINK = 0
+    COLD_DRINK = 1
+    RETURN_CUP = 2
+    GET_BROOM = 3
+    RETURN_BROOM = 4
+
+
+class OrderSiters(Enum):
+    CHANGE_MUSIC = 0
+    MUSIC_VOLUME = 1
+    CHANGE_TEMPERATURE = 3
