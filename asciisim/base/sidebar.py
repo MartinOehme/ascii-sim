b006356a057bffa4deaea7f77483480221d22f06
Karsten Lehmann <mail@kalehmann.de>
2019-12-01 15:06:06 +0100

    feat: Show current item in sidebar

--- asciisim/base/sidebar.py
+++ asciisim/base/sidebar.py
@@ -3,6 +3,7 @@ from pygame import Rect
 
 from .context import Context
 from .game_object import GameObject
+from ..sprites.sprite_enums import OrderWalkers
 from ..res import IMG_DIR
 
 class SideBar(GameObject):
@@ -12,12 +13,38 @@ class SideBar(GameObject):
             "sidebar",
             lambda: pygame.image.load(IMG_DIR + "sidebar.png")
         )
+        self.item = None
+        for icon in OrderWalkers:
+            self.register_surface(
+                icon.value,
+                lambda: pygame.transform.smoothscale(
+                    pygame.image.load(
+                        IMG_DIR + f"speech_bubbles/order_walkers/{icon.value}.png"
+                    ),
+                    (300, 300)
+                )
+            )
         
     @property
     def image(self):
-        return self.get_surface("sidebar")
+        surface = self.get_surface("sidebar").copy()
+        scaling = self.TILE_SIZE / 135
+        
+        if self.item:
+            surface.blit(
+                self.get_surface(self.item.value),
+                Rect(
+                    67 * scaling,
+                    675 * scaling,
+                    300 * scaling,
+                    300 * scaling
+                )
+            )
+        
+        return surface
         
     def update(self, context):
+        self.item = context.bar_keeper.item
         if context.room_key == Context.BAR_ROOM:
             self.is_left = True
         else:
